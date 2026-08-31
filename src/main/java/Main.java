82f8a012b5dfea0574d3d5db3b4502abf2d38972
My2h <thawmyathtoo@email.com>
2026-08-31 19:03:43 +0800

    Build the Duke GUI layout

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -17,8 +18,8 @@ public class Main extends Application {
     private Button sendButton;
     private Scene scene;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/troll-face.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/michael-scott.png"));
 
     @Override
     public void start(Stage stage) {
@@ -41,6 +42,36 @@ public class Main extends Application {
 
          stage.setScene(scene);
          stage.show();
+
+         //Formatting the window to look as expected
+
+         stage.setTitle("Duke");
+         stage.setResizable(false);
+         stage.setMinHeight(600.0);
+         stage.setMinWidth(400.0);
+
+         mainLayout.setPrefSize(400.0, 600.0);
+
+         scrollPane.setPrefSize(385, 535);
+         scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+         scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+         scrollPane.setVvalue(1.0);
+         scrollPane.setFitToWidth(true);
+
+         dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+         userInput.setPrefWidth(325.0);
+
+         sendButton.setPrefWidth(55.0);
+
+         AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+         AnchorPane.setBottomAnchor(sendButton, 1.0);
+         AnchorPane.setRightAnchor(sendButton, 1.0);
+
+         AnchorPane.setLeftAnchor(userInput, 1.0);
+         AnchorPane.setBottomAnchor(userInput, 1.0);
 
          //More code to be added here later
     }
